29d71bd5fed35dc398efaa40b4b9f8f34bb7e5df
Farhaan Nishtar <farhaanmnishtar99@gmail.com>
2023-10-30 12:16:36 -0400

    feat: create fetchDailyFocusTime function to render focusTime state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,23 @@
+"use client"
+import { useState } from "react";
 
 export default function Home() {
+  const [focusTime, setFocusTime] = useState(0);
+  
+  const fetchDailyFocusTime = (e: any) => {
+    e.preventDefault;
+    setFocusTime(4)
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Cleaning up
+    <main className="flex flex-col items-center justify-between p-24">
+      <button
+        onClick={fetchDailyFocusTime}>
+        Fetch focus time
+      </button>
+      {
+        focusTime === 0 ? <div></div> : <div>{focusTime} hours focused past 24 hours</div> 
+      }
     </main>
   )
 }
